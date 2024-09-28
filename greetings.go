a64adaeaf88d04a5733faa450aad0ab4218b2b43
sumancfc <sumanstha999@gmail.com>
2024-09-28 15:27:45 +0200

    handling error if name is empty

--- greetings.go
+++ greetings.go
@@ -1,10 +1,17 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hero returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message
+	if name == "" {
+		return "", errors.New("name is empty")
+	}
 	// Return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
