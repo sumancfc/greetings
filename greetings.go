7b95f681fa0cb3f69595eb0a5f8c97682a782b2b
sumancfc <sumanstha999@gmail.com>
2024-09-28 16:03:16 +0200

    random greeting added

--- greetings.go
+++ greetings.go
@@ -3,7 +3,20 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
+
+func randomFormat() string {
+	// A slice of message format
+	formats := []string{
+		"Hi, %v. Welcome",
+		"Greet to see you, %v",
+		"Hail, %v! Well met!",
+	}
+
+	//return a randomly selected message
+	return formats[rand.Intn(len(formats))]
+}
 
 // Hero returns a greeting for the named person.
 func Hello(name string) (string, error) {
@@ -12,6 +25,6 @@ func Hello(name string) (string, error) {
 		return "", errors.New("name is empty")
 	}
 	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
 }
